d54f281340b9583ffe90b594cb914f45d434908c
Geane Lima Batista <geane@MacBook-Air-de-Geane.local>
2024-11-04 00:20:31 -0300

    todos os testes passando com sucesso amem

--- cypress/e2e/funcionario.cy.js
+++ cypress/e2e/funcionario.cy.js
@@ -3,7 +3,7 @@ import { buttonAddEmployee, clickButtonSave, clickOnPIMoptionOnMenu, createLogin
 import { loginPage } from '../testFunction/login';
 
 describe('Criação,Atualização e deleção de funcionário', () => {
-    
+
 
 
     it('Registrar um novo funcionário', () => {
@@ -36,19 +36,15 @@ describe('Criação,Atualização e deleção de funcionário', () => {
             expect(response.statusCode).to.eq(200);
           });
         clickOnPIMoptionOnMenu()
-        cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-autocomplete-wrapper > .oxd-autocomplete-text-input > input').type('trabalho pratico devops')
+        cy.get(':nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-autocomplete-wrapper > .oxd-autocomplete-text-input > input').type('geanelimabatista pratico devops')
         cy.wait(1000)
         cy.get('.oxd-form-actions > .oxd-button--secondary').click({ force: true })
         cy.wait(2000)
-        cy.get('.oxd-table-cell-actions > :nth-child(2)').click()
+        cy.get('.oxd-table-cell-actions > :nth-child(1)').click()
         cy.get(':nth-child(1) > .oxd-grid-3 > .oxd-grid-item > .oxd-input-group > :nth-child(2) > .oxd-input').type('nuuu')
         cy.get(':nth-child(3) > :nth-child(1) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('0321')
         cy.get(':nth-child(3) > :nth-child(2) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').type('61901-280')
-        cy.get(':nth-child(3) > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').type('41909922')
-        cy.get(':nth-child(3) > :nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type('9929292')
         cy.get(':nth-child(1) > :nth-child(2) > .oxd-radio-wrapper > label > .oxd-radio-input').click({ force: true })
-        cy.get(':nth-child(7) > .oxd-grid-3 > :nth-child(1) > .oxd-input-group > :nth-child(2) > .oxd-input').type('No')
-        cy.get(':nth-child(2) > .oxd-checkbox-wrapper > label > .oxd-checkbox-input > .oxd-icon').click({ force: true })
         cy.get(':nth-child(1) > .oxd-form > .oxd-form-actions > .oxd-button').click()
         cy.get('.oxd-toast').should("include.text", "Successfully Updated")
     })
